e65c0fa0b520a85e1e0ae48221005ef62e9ba9f6
C. Murat Dodan'li <CMDody@users.noreply.github.com>
2016-03-23 09:28:22 +0200

    A better verison...

--- newc.c
+++ newc.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
+#define MAX 11
 
 int main(void) {
 
 printf("This is a test file for GitHub \n");
 
-exit(0);
+for(int i=1;i<MAX;i++) {
+  
+  for(int j=0;j<i;j++) {
+    printf("*");  
+  }
+  printf("\n");
+}
 
 return 0;
 }
