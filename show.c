5556d8857444a3959bbfeeddaca8759685535af6
C. Murat Dodan'li <cmdodanli78@hotmail.com>
2016-03-24 05:08:50 +0200

    Last modifications

--- show.c
+++ show.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 
 int main(void) {
+
+errno = 0;
 
 if(!system("ls -Geal")) {
 printf("Successfully done! \n");
 }
 else {
 printf("Fault! \n");
+}
+
+if(errno==0) {
+printf("No error occured while execution! \n");
+}
+else {
+printf("Error Number:%d \n", errno);
 }
 
 return 0;
